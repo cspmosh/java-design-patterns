a38c76a844fd98e20b8b148f478d66e957f0d48c
Josh Turner <cs.pmosh@gmail.com>
2020-06-23 10:55:27 -0500

    moved field class inside of CodeBuilder class to contain relevent code for each pattern

--- src/CodeBuilder.java
+++ src/CodeBuilder.java
@@ -39,3 +39,26 @@ public class CodeBuilder {
         return sb.toString();
     }
 }
+
+class Field {
+    private final String name;
+    private final String type;
+
+    public Field(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("public %s %s;", type, name);
+    }
+}
